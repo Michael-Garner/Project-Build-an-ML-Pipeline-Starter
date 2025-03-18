7d830f2e884db71779dc191ef78870d96353b162
Michael-Garner <mgarn86@wgu.edu>
2025-03-18 13:46:09 -0500

    Step 6

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Checks data to see if it is within the pre-defined number of rows.
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data,min_price,max_price):
+    """
+    Checks data to see if the price is within the pre-defined range.
+    """
+    assert data['price'].between(min_price,max_price,inclusive='both').all()
